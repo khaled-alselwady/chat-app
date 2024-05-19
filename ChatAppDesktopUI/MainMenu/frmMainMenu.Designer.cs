a897007ce88bd7a843649d1f2752dc3e3173f92b
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-19 06:20:20 +0300

    UPD the icon of frmMainMenu

--- ChatAppDesktopUI/MainMenu/frmMainMenu.Designer.cs
+++ ChatAppDesktopUI/MainMenu/frmMainMenu.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMainMenu));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panelChats = new System.Windows.Forms.Panel();
             this.panelNoLContentsExist = new System.Windows.Forms.Panel();
@@ -252,6 +253,7 @@
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "frmMainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
