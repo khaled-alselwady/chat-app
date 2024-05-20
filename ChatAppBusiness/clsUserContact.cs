28e81178493c149648f70850f3158a6315215655
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-20 06:46:41 +0300

    UPD clsUserContact & clsUserContactData

--- ChatAppBusiness/clsUserContact.cs
+++ ChatAppBusiness/clsUserContact.cs
@@ -4,84 +4,95 @@ using System.Data;
 
 namespace ChatAppBusiness
 {
-public class clsUserContact
-{
-public enum enMode { AddNew = 0, Update = 1 };
-public enMode Mode = enMode.AddNew;
+    public class clsUserContact
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
 
-public int? UserContactID { get; set; }
-public int UserID { get; set; }
-public int ContactID { get; set; }
+        public int? UserContactID { get; set; }
+        public int? UserID { get; set; }
+        public int? ContactID { get; set; }
+        public DateTime CreatedDate { get; set; }
 
-public clsUserContact()
-{
-    UserContactID = null;
-    UserID = -1;
-    ContactID = -1;
+        public clsUser UserInfo { get; private set; }
+        public clsUser ContactInfo { get; private set; }
 
-    Mode = enMode.AddNew;
-}
+        public clsUserContact()
+        {
+            UserContactID = null;
+            UserID = null;
+            ContactID = null;
+            CreatedDate = DateTime.Now;
 
-private clsUserContact(int? userContactID, int userID, int contactID)
-{
-    UserContactID = userContactID;
-    UserID = userID;
-    ContactID = contactID;
+            Mode = enMode.AddNew;
+        }
 
-    Mode = enMode.Update;
-}
+        private clsUserContact(int? userContactID, int? userID, int? contactID, DateTime createdDate)
+        {
+            UserContactID = userContactID;
+            UserID = userID;
+            ContactID = contactID;
+            CreatedDate = createdDate;
 
-private bool _Add()
-{
-    UserContactID = clsUserContactData.Add(UserID, ContactID);
+            UserInfo = clsUser.Find(userID);
+            ContactInfo = clsUser.Find(contactID);
 
-    return (UserContactID.HasValue);
-}
+            Mode = enMode.Update;
+        }
 
-private bool _Update()
-{
-return clsUserContactData.Update(UserContactID, UserID, ContactID);
-}
+        private bool _Add()
+        {
+            UserContactID = clsUserContactData.Add(UserID.Value, ContactID.Value);
 
-public bool Save()
-{
-switch (Mode)
-{
-case enMode.AddNew:
-if (_Add())
-{
-Mode = enMode.Update;
-return true;
-}
-else
-{
-return false;
-}
+            return (UserContactID.HasValue);
+        }
 
-case enMode.Update:
-return _Update();
-}
+        private bool _Update()
+        {
+            return clsUserContactData.Update(UserContactID.Value, UserID.Value, ContactID.Value);
+        }
 
-return false;
-}
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_Add())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
 
-public static clsUserContact Find(int? userContactID)
-{
-int userID = -1;
-    int contactID = -1;
+                case enMode.Update:
+                    return _Update();
+            }
 
-bool isFound = clsUserContactData.GetInfoByID(userContactID, ref userID, ref contactID);
+            return false;
+        }
 
-return (isFound) ? (new clsUserContact(userContactID, userID, contactID)) : null;
-}
+        public static clsUserContact Find(int? userContactID)
+        {
+            int? userID = null;
+            int? contactID = null;
+            DateTime createdDate = DateTime.Now;
 
-public static bool Delete(int? userContactID)
-=> clsUserContactData.Delete(userContactID);
+            bool isFound = clsUserContactData.GetInfoByID(userContactID, ref userID,
+                ref contactID, ref createdDate);
 
-public static bool Exists(int? userContactID)
-=> clsUserContactData.Exists(userContactID);
+            return (isFound) ? (new clsUserContact(userContactID, userID, contactID, createdDate)) : null;
+        }
 
-public static DataTable All()
-=> clsUserContactData.All();
-}
+        public static bool Delete(int? userContactID)
+        => clsUserContactData.Delete(userContactID);
+
+        public static bool Exists(int? userContactID)
+        => clsUserContactData.Exists(userContactID);
+
+        public static DataTable All()
+        => clsUserContactData.All();
+    }
 }
--- ChatAppDataAccess/clsUserContactData.cs
+++ ChatAppDataAccess/clsUserContactData.cs
@@ -4,127 +4,129 @@ using System.Data.SqlClient;
 
 namespace ChatAppDataAccess
 {
-public class clsUserContactData
-{
-public static bool GetInfoByID(int? userContactID, ref int userID, ref int contactID)
-{
-    bool isFound = false;
-
-    try
+    public class clsUserContactData
     {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+        public static bool GetInfoByID(int? userContactID, ref int? userID,
+            ref int? contactID, ref DateTime createdDate)
         {
-            connection.Open();
+            bool isFound = false;
 
-            using (SqlCommand command = new SqlCommand("SP_GetUserContactInfoByID", connection))
+            try
             {
-command.CommandType = CommandType.StoredProcedure;
-
-                command.Parameters.AddWithValue("@UserContactID", (object)userContactID ?? DBNull.Value);
-
-                using (SqlDataReader reader = command.ExecuteReader())
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
                 {
-                    if (reader.Read())
-                    {
-                        // The record was found
-                        isFound = true;
+                    connection.Open();
 
-userID = (int)reader["UserID"];
-contactID = (int)reader["ContactID"];
-                    }
-                    else
+                    using (SqlCommand command = new SqlCommand("SP_GetUserContactInfoByID", connection))
                     {
-                        // The record was not found
-                        isFound = false;
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@UserContactID", (object)userContactID ?? DBNull.Value);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // The record was found
+                                isFound = true;
+
+                                userID = (reader["UserID"] != DBNull.Value) ? (int?)reader["UserID"] : null;
+                                contactID = (reader["ContactID"] != DBNull.Value) ? (int?)reader["ContactID"] : null;
+                                createdDate = (DateTime)reader["CreatedDate"];
+                            }
+                            else
+                            {
+                                // The record was not found
+                                isFound = false;
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-catch (Exception ex)
+            catch (Exception ex)
             {
                 isFound = false;
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return isFound;
-}
-
-public static int? Add(int userID, int contactID)
-{
-// This function will return the new person id if succeeded and null if not
-    int? userContactID = null;
-
-    try
-    {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
-        {
-            connection.Open();
-
-            using (SqlCommand command = new SqlCommand("SP_AddNewUserContact", connection))
-            {
-command.CommandType = CommandType.StoredProcedure;
-
-command.Parameters.AddWithValue("@UserID", userID);
-command.Parameters.AddWithValue("@ContactID", contactID);
-
-SqlParameter outputIdParam = new SqlParameter("@NewUserContactID", SqlDbType.Int)
-{
-Direction = ParameterDirection.Output
-};
-command.Parameters.Add(outputIdParam);
-
-command.ExecuteNonQuery();
-
-userContactID = (int?)outputIdParam.Value;
-            }
+            return isFound;
         }
-    }
-catch (Exception ex)
+
+        public static int? Add(int userID, int contactID)
+        {
+            // This function will return the new person id if succeeded and null if not
+            int? userContactID = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_AddNewUserContact", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@UserID", userID);
+                        command.Parameters.AddWithValue("@ContactID", contactID);
+
+                        SqlParameter outputIdParam = new SqlParameter("@NewUserContactID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        userContactID = (int?)outputIdParam.Value;
+                    }
+                }
+            }
+            catch (Exception ex)
             {
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return userContactID;
-}
-
-public static bool Update(int? userContactID, int userID, int contactID)
-{
-    int rowAffected = 0;
-
-    try
-    {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
-        {
-            connection.Open();
-
-            using (SqlCommand command = new SqlCommand("SP_UpdateUserContact", connection))
-            {
-command.CommandType = CommandType.StoredProcedure;
-
-                command.Parameters.AddWithValue("@UserContactID", (object)userContactID ?? DBNull.Value);
-command.Parameters.AddWithValue("@UserID", userID);
-command.Parameters.AddWithValue("@ContactID", contactID);
-
-                rowAffected = command.ExecuteNonQuery();
-            }
+            return userContactID;
         }
-    }
-catch (Exception ex)
+
+        public static bool Update(int userContactID, int userID, int contactID)
+        {
+            int rowAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_UpdateUserContact", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@UserContactID", userContactID);
+                        command.Parameters.AddWithValue("@UserID", userID);
+                        command.Parameters.AddWithValue("@ContactID", contactID);
+
+                        rowAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
             {
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return (rowAffected > 0);
-}
+            return (rowAffected > 0);
+        }
 
-public static bool Delete(int? userContactID)
-=> clsDataAccessHelper.Delete("SP_DeleteUserContact", "UserContactID", userContactID);
+        public static bool Delete(int? userContactID)
+        => clsDataAccessHelper.Delete("SP_DeleteUserContact", "UserContactID", userContactID);
 
-public static bool Exists(int? userContactID)
-=> clsDataAccessHelper.Exists("SP_DoesUserContactExist", "UserContactID", userContactID);
+        public static bool Exists(int? userContactID)
+        => clsDataAccessHelper.Exists("SP_DoesUserContactExist", "UserContactID", userContactID);
 
-public static DataTable All()
-=> clsDataAccessHelper.All("SP_GetAllUserContacts");
-}
+        public static DataTable All()
+        => clsDataAccessHelper.All("SP_GetAllUserContacts");
+    }
 }
