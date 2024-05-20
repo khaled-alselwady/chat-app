d18da5edc99b0a8937aae1514a1f831d0ff60ab3
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-05-20 06:19:01 +0300

    UPD clsMessage & clsMessageData

--- ChatAppBusiness/clsMessage.cs
+++ ChatAppBusiness/clsMessage.cs
@@ -9,26 +9,40 @@ namespace ChatAppBusiness
         public enum enMode { AddNew = 0, Update = 1 };
         public enMode Mode = enMode.AddNew;
 
+        public enum enStatus
+        {
+            Sent = 1,
+            Delivered = 2,
+            Read = 3,
+            Deleted = 4,
+        }
+
         public int? MessageID { get; set; }
-        public int SenderID { get; set; }
-        public int RecipientID { get; set; }
+        public int? SenderID { get; set; }
+        public int? RecipientID { get; set; }
         public string MessageContent { get; set; }
         public DateTime MessageDate { get; set; }
-        public byte Status { get; set; }
+        public enStatus Status { get; set; }
+
+        public string StatusText => _GetStatusText();
+
+        public clsUser SenderInfo { get; private set; }
+        public clsUser RecipientInfo { get; private set; }
 
         public clsMessage()
         {
             MessageID = null;
-            SenderID = -1;
-            RecipientID = -1;
+            SenderID = null;
+            RecipientID = null;
             MessageContent = string.Empty;
             MessageDate = DateTime.Now;
-            Status = 0;
+            Status = enStatus.Sent;
 
             Mode = enMode.AddNew;
         }
 
-        private clsMessage(int? messageID, int senderID, int recipientID, string messageContent, DateTime messageDate, byte status)
+        private clsMessage(int? messageID, int? senderID, int? recipientID, string messageContent,
+            DateTime messageDate, enStatus status)
         {
             MessageID = messageID;
             SenderID = senderID;
@@ -37,19 +51,44 @@ namespace ChatAppBusiness
             MessageDate = messageDate;
             Status = status;
 
+            SenderInfo = clsUser.Find(senderID);
+            RecipientInfo = clsUser.Find(recipientID);
+
             Mode = enMode.Update;
+        }
+
+        private string _GetStatusText()
+        {
+            switch (Status)
+            {
+                case enStatus.Sent:
+                    return "Sent";
+
+                case enStatus.Delivered:
+                    return "Delivered";
+
+                case enStatus.Read:
+                    return "Read";
+
+                case enStatus.Deleted:
+                    return "Deleted";
+
+                default:
+                    return "Sent";
+            }
         }
 
         private bool _Add()
         {
-            MessageID = clsMessageData.Add(SenderID, RecipientID, MessageContent, MessageDate, Status);
+            MessageID = clsMessageData.Add(SenderID.Value, RecipientID.Value, MessageContent, MessageDate, (byte)Status);
 
             return (MessageID.HasValue);
         }
 
         private bool _Update()
         {
-            return clsMessageData.Update(MessageID, SenderID, RecipientID, MessageContent, MessageDate, Status);
+            return clsMessageData.Update(MessageID.Value, SenderID.Value, RecipientID.Value,
+                MessageContent, MessageDate, (byte)Status);
         }
 
         public bool Save()
@@ -76,15 +115,18 @@ namespace ChatAppBusiness
 
         public static clsMessage Find(int? messageID)
         {
-            int senderID = -1;
-            int recipientID = -1;
+            int? senderID = null;
+            int? recipientID = null;
             string messageContent = string.Empty;
             DateTime messageDate = DateTime.Now;
             byte status = 0;
 
-            bool isFound = clsMessageData.GetInfoByID(messageID, ref senderID, ref recipientID, ref messageContent, ref messageDate, ref status);
+            bool isFound = clsMessageData.GetInfoByID(messageID, ref senderID, ref recipientID,
+                ref messageContent, ref messageDate, ref status);
 
-            return (isFound) ? (new clsMessage(messageID, senderID, recipientID, messageContent, messageDate, status)) : null;
+            return (isFound) ? (new clsMessage(messageID, senderID, recipientID,
+                                messageContent, messageDate, (enStatus)status))
+                             : null;
         }
 
         public static bool Delete(int? messageID)
--- ChatAppDataAccess/clsMessageData.cs
+++ ChatAppDataAccess/clsMessageData.cs
@@ -4,136 +4,139 @@ using System.Data.SqlClient;
 
 namespace ChatAppDataAccess
 {
-public class clsMessageData
-{
-public static bool GetInfoByID(int? messageID, ref int senderID, ref int recipientID, ref string messageContent, ref DateTime messageDate, ref byte status)
-{
-    bool isFound = false;
-
-    try
+    public class clsMessageData
     {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+        public static bool GetInfoByID(int? messageID, ref int? senderID, ref int? recipientID,
+            ref string messageContent, ref DateTime messageDate, ref byte status)
         {
-            connection.Open();
+            bool isFound = false;
 
-            using (SqlCommand command = new SqlCommand("SP_GetMessageInfoByID", connection))
+            try
             {
-command.CommandType = CommandType.StoredProcedure;
-
-                command.Parameters.AddWithValue("@MessageID", (object)messageID ?? DBNull.Value);
-
-                using (SqlDataReader reader = command.ExecuteReader())
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
                 {
-                    if (reader.Read())
-                    {
-                        // The record was found
-                        isFound = true;
+                    connection.Open();
 
-senderID = (int)reader["SenderID"];
-recipientID = (int)reader["RecipientID"];
-messageContent = (string)reader["MessageContent"];
-messageDate = (DateTime)reader["MessageDate"];
-status = (byte)reader["Status"];
-                    }
-                    else
+                    using (SqlCommand command = new SqlCommand("SP_GetMessageInfoByID", connection))
                     {
-                        // The record was not found
-                        isFound = false;
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@MessageID", (object)messageID ?? DBNull.Value);
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                // The record was found
+                                isFound = true;
+
+                                senderID = (reader["SenderID"] != DBNull.Value) ? (int?)reader["SenderID"] : null;
+                                recipientID = (reader["RecipientID"] != DBNull.Value) ? (int?)reader["RecipientID"] : null;
+                                messageContent = (string)reader["MessageContent"];
+                                messageDate = (DateTime)reader["MessageDate"];
+                                status = (byte)reader["Status"];
+                            }
+                            else
+                            {
+                                // The record was not found
+                                isFound = false;
+                            }
+                        }
                     }
                 }
             }
-        }
-    }
-catch (Exception ex)
+            catch (Exception ex)
             {
                 isFound = false;
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return isFound;
-}
-
-public static int? Add(int senderID, int recipientID, string messageContent, DateTime messageDate, byte status)
-{
-// This function will return the new person id if succeeded and null if not
-    int? messageID = null;
-
-    try
-    {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
-        {
-            connection.Open();
-
-            using (SqlCommand command = new SqlCommand("SP_AddNewMessage", connection))
-            {
-command.CommandType = CommandType.StoredProcedure;
-
-command.Parameters.AddWithValue("@SenderID", senderID);
-command.Parameters.AddWithValue("@RecipientID", recipientID);
-command.Parameters.AddWithValue("@MessageContent", messageContent);
-command.Parameters.AddWithValue("@MessageDate", messageDate);
-command.Parameters.AddWithValue("@Status", status);
-
-SqlParameter outputIdParam = new SqlParameter("@NewMessageID", SqlDbType.Int)
-{
-Direction = ParameterDirection.Output
-};
-command.Parameters.Add(outputIdParam);
-
-command.ExecuteNonQuery();
-
-messageID = (int?)outputIdParam.Value;
-            }
+            return isFound;
         }
-    }
-catch (Exception ex)
+
+        public static int? Add(int senderID, int recipientID, string messageContent,
+            DateTime messageDate, byte status)
+        {
+            // This function will return the new person id if succeeded and null if not
+            int? messageID = null;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_AddNewMessage", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@SenderID", senderID);
+                        command.Parameters.AddWithValue("@RecipientID", recipientID);
+                        command.Parameters.AddWithValue("@MessageContent", messageContent);
+                        command.Parameters.AddWithValue("@MessageDate", messageDate);
+                        command.Parameters.AddWithValue("@Status", status);
+
+                        SqlParameter outputIdParam = new SqlParameter("@NewMessageID", SqlDbType.Int)
+                        {
+                            Direction = ParameterDirection.Output
+                        };
+                        command.Parameters.Add(outputIdParam);
+
+                        command.ExecuteNonQuery();
+
+                        messageID = (int?)outputIdParam.Value;
+                    }
+                }
+            }
+            catch (Exception ex)
             {
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return messageID;
-}
-
-public static bool Update(int? messageID, int senderID, int recipientID, string messageContent, DateTime messageDate, byte status)
-{
-    int rowAffected = 0;
-
-    try
-    {
-        using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
-        {
-            connection.Open();
-
-            using (SqlCommand command = new SqlCommand("SP_UpdateMessage", connection))
-            {
-command.CommandType = CommandType.StoredProcedure;
-
-                command.Parameters.AddWithValue("@MessageID", (object)messageID ?? DBNull.Value);
-command.Parameters.AddWithValue("@SenderID", senderID);
-command.Parameters.AddWithValue("@RecipientID", recipientID);
-command.Parameters.AddWithValue("@MessageContent", messageContent);
-command.Parameters.AddWithValue("@MessageDate", messageDate);
-command.Parameters.AddWithValue("@Status", status);
-
-                rowAffected = command.ExecuteNonQuery();
-            }
+            return messageID;
         }
-    }
-catch (Exception ex)
+
+        public static bool Update(int messageID, int senderID, int recipientID,
+            string messageContent, DateTime messageDate, byte status)
+        {
+            int rowAffected = 0;
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand("SP_UpdateMessage", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@MessageID", messageID);
+                        command.Parameters.AddWithValue("@SenderID", senderID);
+                        command.Parameters.AddWithValue("@RecipientID", recipientID);
+                        command.Parameters.AddWithValue("@MessageContent", messageContent);
+                        command.Parameters.AddWithValue("@MessageDate", messageDate);
+                        command.Parameters.AddWithValue("@Status", status);
+
+                        rowAffected = command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
             {
                 clsDataAccessHelper.HandleException(ex);
             }
 
-    return (rowAffected > 0);
-}
+            return (rowAffected > 0);
+        }
 
-public static bool Delete(int? messageID)
-=> clsDataAccessHelper.Delete("SP_DeleteMessage", "MessageID", messageID);
+        public static bool Delete(int? messageID)
+        => clsDataAccessHelper.Delete("SP_DeleteMessage", "MessageID", messageID);
 
-public static bool Exists(int? messageID)
-=> clsDataAccessHelper.Exists("SP_DoesMessageExist", "MessageID", messageID);
+        public static bool Exists(int? messageID)
+        => clsDataAccessHelper.Exists("SP_DoesMessageExist", "MessageID", messageID);
 
-public static DataTable All()
-=> clsDataAccessHelper.All("SP_GetAllMessages");
-}
+        public static DataTable All()
+        => clsDataAccessHelper.All("SP_GetAllMessages");
+    }
 }
